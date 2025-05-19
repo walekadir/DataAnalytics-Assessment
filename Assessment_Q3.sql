52f2c2a45d7a879d235ddb475a84a9fcab3793ae
walekadir <62019833+walekadir@users.noreply.github.com>
2025-05-19 12:41:53 +0100

    Create Assessment_Q3.sql

---
+++ Assessment_Q3.sql
@@ -0,0 +1,41 @@
+SELECT
+    id AS plan_id,
+    owner_id,
+    'Savings' AS type,
+    last_transaction_date,
+    DATEDIFF(DAY, last_transaction_date, GETDATE()) AS inactivity_days
+FROM (
+    SELECT
+        id,
+        owner_id,
+        MAX(transaction_date) AS last_transaction_date
+    FROM
+        savings_savingsaccount
+    WHERE
+        is_regular_savings = 1 AND confirmed_amount > 0
+    GROUP BY
+        id, owner_id
+    HAVING
+        MAX(transaction_date) < DATEADD(DAY, -365, GETDATE()) OR MAX(transaction_date) IS NULL
+) savings
+UNION
+SELECT
+    id AS plan_id,
+    owner_id,
+    'Investment' AS type,
+    last_transaction_date,
+    DATEDIFF(DAY, last_transaction_date, GETDATE()) AS inactivity_days
+FROM (
+    SELECT
+        id,
+        owner_id,
+        MAX(transaction_date) AS last_transaction_date
+    FROM
+        plans_plan
+    WHERE
+        is_a_fund = 1 AND confirmed_amount > 0
+    GROUP BY
+        id, owner_id
+    HAVING
+        MAX(transaction_date) < DATEADD(DAY, -365, GETDATE()) OR MAX(transaction_date) IS NULL
+) investments;
